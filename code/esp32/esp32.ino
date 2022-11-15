045c49e97412063b61a17f7e21b2d3d1c21ba5b4
lucusowl <lucusowl@gmail.com>
2022-11-16 07:01:53 +0900

    Update esp32.ino

--- code/esp32/esp32.ino
+++ code/esp32/esp32.ino
@@ -1,25 +1,55 @@
 /**
  * @file esp32.ino
  * @author switch-team, siorTeam
- * @version 0.1.0
- * @date 2022-10-28
+ * @version 0.3.0
+ * @date 2022-11-15
  * 
  * @copyright Copyright (c) 2022
  * 
  */
 
-#include <ESP32Servo.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
-Servo serv;  // servo object
- 
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+BLEServer         *pServer;
+BLEService        *pService;
+BLECharacteristic *pCharacteristic;
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string value = pCharacteristic->getValue();
+
+    if (value.length() > 0) {
+      Serial.print("FROM MOBILE: ");
+      for (int i = 0; i < value.length(); i++) {
+        Serial.print(value[i]);
+      }
+      Serial.println();
+    }
+  }
+};
+
 void setup() {
-  serv.setPeriodHertz(50); 
-  serv.attach(13);
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("remote-SW");
+  pServer         = BLEDevice::createServer();
+  pService        = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
 }
  
 void loop() {
-  serv.write(0); 
-  delay(2000);
-  serv.write(180);
   delay(2000);
 }
